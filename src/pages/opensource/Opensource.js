96758bfc8812fb73b37e2b6a52ede0339554d4c4
Amit Salunkhe <salunkheamit22@gmail.com>
2020-05-28 22:26:33 +0530

    First Commit

---
+++ src/pages/opensource/Opensource.js
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import OpensourceCharts from "../../containers/opensourceCharts/OpensourceCharts";
+import Organizations from "../../containers/organizations/Organizations";
+//import PullRequests from "../../containers/pullRequests/PullRequests";
+//import Issues from '../../containers/issues/Issues';
+import "./Opensource.css";
+
+
+class Opensource extends Component {
+    render() {
+        return (
+					<div className="opensource-main">
+						<Header />
+						<Organizations />
+						<OpensourceCharts />
+						
+						<Footer />
+					</div>
+				);
+    }
+}
+
+export default Opensource;
+
+/*<PullRequests />
+  <Issues />*/
