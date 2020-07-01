cff69089d6d49232c4101bca2d581c1e26662e51
Amit Salunkhe <salunkheamit22@gmail.com>
2020-07-01 14:48:07 +0530

    Blogsite Commited

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -14,7 +14,8 @@ const blogSection = {
 	title: "Blogs",
 	subtitle:
 		"For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-	link: "http://34.68.249.152/",
+	link: "https://www.codewithamit.tk/", //"http://34.68.249.152/",
+
 	avatar_image_path: "blogs_image.svg"
 };
 
